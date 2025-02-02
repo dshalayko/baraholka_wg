101a44feab8aef570d8e1f0546d34a558ec8a018
dmitryshalayko <dshalayko@gmail.com>
2025-02-02 12:23:46 +0100

    pizdec nahui2

--- run_userbot.py
+++ run_userbot.py
@@ -6,7 +6,9 @@ from pyrogram.enums import ChatType
 from config import API_ID, API_HASH
 
 ENV_FILE = ".env"
-
+#-1002335133236
+#
+#-1002288054679
 async def get_supergroup_id():
     async with Client("my_session", api_id=API_ID, api_hash=API_HASH) as app:
         print("Получаем список чатов...")
@@ -15,30 +17,12 @@ async def get_supergroup_id():
             chat = dialog.chat
             if chat.type == ChatType.SUPERGROUP:
                 print(f"Найдена супергруппа: {chat.title} (ID: {chat.id})")
-                save_chat_id_to_env(chat.id)
                 return
 
         print("Не найдено ни одной супергруппы!")
 
 
-def save_chat_id_to_env(chat_id):
-    current_chat_id = None
 
-    # Читаем текущий CHAT_ID
-    if os.path.exists(ENV_FILE):
-        with open(ENV_FILE, "r") as env_file:
-            for line in env_file:
-                if line.startswith("CHAT_ID="):
-                    current_chat_id = line.strip().split("=")[1]
-                    break
-
-    if current_chat_id == str(chat_id):
-        print(f"CHAT_ID уже установлен: {chat_id}")
-    else:
-        # Перезаписываем файл .env
-        with open(ENV_FILE, "w") as env_file:
-            env_file.write(f"CHAT_ID={chat_id}\n")
-        print(f"CHAT_ID обновлён: {chat_id}")
 
 
 asyncio.run(get_supergroup_id())
