2ef7eb5d44749afcaf7745841bfb500bbaebc47c
d_shalayko <dshalayko@gmail.com>
2026-01-31 23:00:41 +0100

    порт 8001 для запуска

--- webserver/__main__.py
+++ webserver/__main__.py
@@ -2,7 +2,7 @@ import uvicorn
 
 
 def main() -> None:
-    uvicorn.run("webserver.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("webserver.app:app", host="0.0.0.0", port=8001, reload=True)
 
 
 if __name__ == "__main__":
